9c64110fb1c30d03dda6d141f31af2e9a3bea227
rohankbd <rohankbd.1@gmail.com>
2023-10-02 21:06:48 +0530

    Added Vercel analytics

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 ReactDOM.render(<App />, document.getElementById("root"));
